d3f4237ea6a2868cee99f23d79fdad090b82551c
Nick Daria <me@nickdaria.com>
2025-03-11 01:46:37 -0500

    Additional macros

--- array.h
+++ array.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #define ARR_SIZE(x) (sizeof(x) / sizeof(x[0]))
+#define ARR_INDEX_CHECK(arr, idx) (idx >= 0 && idx < ARR_SIZE(arr))
--- timing.h
+++ timing.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#define TIME_INCREMENT_CLAMP(source, offset, maximum) \
+    (((maximum) - (source) < (offset)) ? (maximum) : ((source) + (offset)))
+
+#define TIME_DECREMENT_CLAMP(source, offset, minimum) \
+    (((source) - (minimum) < (offset)) ? (minimum) : ((source) - (offset)))
+
+
 #define TIME_OFFSET(source, offset) (source + offset)
-#define TIME_PASSED(now, target) ((int64_t)((now) - (target)) >= 0)
+#define TIME_PASSED(now, target) ((int32_t)((now) - (target)) >= 0)
 
 #define uS_TO_MS(us) ((us) / 1000)
 #define MS_TO_uS(ms) ((ms) * 1000)
 
 #define TIME_FLIP_FLOP(now, interval) ((now / interval) % 2)
+#define TIME_INTERVAL(now, interval) ((now % interval) == 0)
